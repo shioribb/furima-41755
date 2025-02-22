2f15d88f3fd81dc87d6b7d167865ad6c9df91766
shioribb <ballet.onigiri@gmail.com>
2025-02-22 15:40:46 +0900

    Basic認証2

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_20_113052) do
-  create_table "items", charset: "utf8mb3", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.1].define(version: 0) do
 end

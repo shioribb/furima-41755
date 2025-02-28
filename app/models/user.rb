3eca8584c282a7c2d324cffcdd10af3820f8f3e9
shioribb <ballet.onigiri@gmail.com>
2025-02-28 12:52:38 +0900

    正規表現のバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_reading, presence: true
-  validates :last_name_reading, presence: true
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角で入力してください。' }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角で入力してください。' }
+  validates :first_name_reading, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナで入力してください。' }
+  validates :last_name_reading, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナで入力してください。' }
   validates :birth_date, presence: true
 
 end

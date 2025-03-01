f9015f99d9ab64da0902fe7c58cf7070b5027311
shioribb <ballet.onigiri@gmail.com>
2025-03-01 18:17:45 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,9 +10,14 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shopping_date
 
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :postage_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :shopping_date_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :category_id, presence: true, numericality: { other_than: 1 , message: "選択してください"}
+  validates :status_id, presence: true, numericality: { other_than: 1 , message: "選択してください"}
+  validates :postage_id, presence: true, numericality: { other_than: 1 , message: "選択してください"}
+  validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "選択してください"}
+  validates :shopping_date_id, presence: true, numericality: { other_than: 1 , message: "選択してください"}
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "は半角で入力してください"}
+  validates :image, presence: true
+
 end

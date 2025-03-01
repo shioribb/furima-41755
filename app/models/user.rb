b0bb10c07ff291229fe4ef68dace73ff86fe365b
shioribb <ballet.onigiri@gmail.com>
2025-03-01 16:57:28 +0900

    itemsテーブル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,7 @@ class User < ApplicationRecord
 
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は半角英数字混合で入力してください' },
                        if: :password_required?
+  
+  has_many :items
+  has_many :buyers
 end

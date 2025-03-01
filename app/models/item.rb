43f2c149c0cde4f4faa133a270001e0992d30840
shioribb <ballet.onigiri@gmail.com>
2025-03-01 17:52:34 +0900

    ActiveHash

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,17 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :buyer
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :postage
+  belongs_to :prefecture
+  belongs_to :shopping_date
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :postage_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shopping_date_id, numericality: { other_than: 1 , message: "can't be blank"}
 end

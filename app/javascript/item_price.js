ab1e8510788c35a8a3a18a2ef2178b93ca11feae
shioribb <ballet.onigiri@gmail.com>
2025-03-01 21:18:01 +0900

    金額入力時の計算機能

---
+++ app/javascript/item_price.js
@@ -0,0 +1,11 @@
+window.addEventListener('turbo:load', () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.round(priceInput.value * 0.1 );
+    const addPofitDom = document.getElementById("profit");
+    addPofitDom.innerHTML = Math.round(priceInput.value - Math.round(priceInput.value * 0.1 ))
+  })
+});

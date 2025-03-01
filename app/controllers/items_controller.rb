d7ab8d2fb6bf99ba606c937e957896c9fde055f0
shioribb <ballet.onigiri@gmail.com>
2025-03-01 19:06:02 +0900

    出品商品の保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,9 +9,17 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
-
+  def item_params
+    params.require(:item).permit(:name, :category_id, :description, :status_id, :postage_id, :prefecture_id, :shopping_date_id, :price, :image ).merge(user_id: current_user.id)
+  end
 end

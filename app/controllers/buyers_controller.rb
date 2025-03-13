62e1a300d9bb27e4564ac23a7e97cbe27e5ae25f
shioribb <ballet.onigiri@gmail.com>
2025-03-13 19:45:40 +0900

    フォームの情報の保存機能

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,6 +1,28 @@
 class BuyersController < ApplicationController
+  before_action :set_item, only:[:index, :create]
+
   def index
-    @item = Item.find(params[:item_id])
     @buyer_address = BuyerAddress.new
   end
+
+  def create
+    @buyer_address = BuyerAddress.new(buyer_params)
+    if @buyer_address.valid?
+      @buyer_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+  
+  def buyer_params
+    params.require(:buyer_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number ).merge(user_id: current_user.id, item_id: @item.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
 end
